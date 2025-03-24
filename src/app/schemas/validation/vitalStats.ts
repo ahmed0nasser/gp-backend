9d5c47b2b53b66a02e52cba4b8bae60194453f66
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-03-24 02:12:23 +0200

    Add vitalStats query validation schema

--- src/app/schemas/validation/vitalStats.ts
+++ src/app/schemas/validation/vitalStats.ts
@@ -1,8 +1,6 @@
 import Joi from "joi";
 
-export const vitalStatSchema = Joi.object({});
-
 export const vitalStatsQuerySchema = Joi.object({
   duration: Joi.string().valid("30mins", "hour", "day", "week").required(),
-  limit: Joi.number().required().min(1).max(50),
+  limit: Joi.number().min(1).max(50),
 });
