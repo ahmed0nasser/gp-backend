c2460f8d6101449cde2642da470c61a750c38e2e
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-03-24 17:57:25 +0200

    Add errorHandler middleware

---
+++ src/app/middleware/errorHandler.ts
@@ -0,0 +1,15 @@
+import { ErrorRequestHandler } from "express";
+import APIError from "../errors/APIError";
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+  if (err instanceof APIError) {
+    res.status(err.statusCode).json({ status: "error", error: err.errRes });
+  } else {
+    res.status(500).json({ status: "error", error: err });
+  }
+};
+
+export default errorHandler;
