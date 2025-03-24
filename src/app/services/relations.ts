7fbabe8d5015e9f57664805cda8a4ddb6b8ff59e
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-03-24 02:16:00 +0200

    Add sendRelationRequest, changeRelationStatus and deleteRelation services

--- src/app/services/relations.ts
+++ src/app/services/relations.ts
@@ -1,5 +1,7 @@
+import { getNextSequence } from "../models/counter";
 import User from "../models/user";
-import { RelationType } from "../schemas/database/relation";
+import { IRelation, RelationType } from "../schemas/database/relation";
+import { IUser } from "../schemas/database/user";
 import { getUserInfoById, UserInfo } from "./users";
 
 export interface UserRelation {
@@ -7,6 +9,8 @@ export interface UserRelation {
   type: RelationType;
   user?: UserInfo;
 }
+
+export type RelationStatus = "accepted" | "rejected" | "canceled";
 
 export const getRelationsByUserId = async (
   userId: number
@@ -29,4 +33,114 @@ export const getRelationsByUserId = async (
   );
 
   return userRelations;
+};
+
+export const sendRelationRequest = async (
+  senderId: number,
+  receiverId: number
+): Promise<number> => {
+  const sender = await User.findById(senderId, "_id relations");
+  if (!sender) {
+    throw new Error();
+  }
+  if (
+    sender.relations.some((relation) => relation.relatedUserId === receiverId)
+  ) {
+    throw new Error();
+  }
+  const receiver = await User.findById(receiverId, "_id relations");
+  if (!receiver) {
+    throw new Error();
+  }
+
+  const newRelationdId = await getNextSequence("relation");
+  sender.relations.push({
+    _id: newRelationdId,
+    relatedUserId: receiverId,
+    type: "outgoing",
+  });
+  receiver.relations.push({
+    _id: newRelationdId,
+    relatedUserId: senderId,
+    type: "incoming",
+  });
+
+  await sender.save();
+  await receiver.save();
+
+  return newRelationdId;
+};
+
+export const changeRelationStatus = async (
+  userId: number,
+  relationId: number,
+  status: RelationStatus
+) => {
+  const { user: initiator, relation: initiatorRelation } =
+    await getUserRelationPair(userId, relationId);
+  const { user: affected, relation: affectedRelation } =
+    await getUserRelationPair(initiatorRelation.relatedUserId, relationId);
+
+  switch (status) {
+    case "accepted":
+      if (initiatorRelation.type !== "incoming") {
+        throw new Error();
+      }
+      initiatorRelation.type = "relation";
+      affectedRelation.type = "relation";
+      break;
+    case "rejected":
+      if (initiatorRelation.type !== "incoming") {
+        throw new Error();
+      }
+      deleteUserRelation(initiator, initiatorRelation);
+      deleteUserRelation(affected, affectedRelation);
+      break;
+    case "canceled":
+      if (initiatorRelation.type !== "outgoing") {
+        throw new Error();
+      }
+      deleteUserRelation(initiator, initiatorRelation);
+      deleteUserRelation(affected, affectedRelation);
+      break;
+  }
+
+  await initiator.save();
+  await affected.save();
+};
+
+export const deleteRelation = async (userId: number, relationId: number) => {
+  const { user: initiator, relation: initiatorRelation } =
+    await getUserRelationPair(userId, relationId);
+  const { user: affected, relation: affectedRelation } =
+    await getUserRelationPair(initiatorRelation.relatedUserId, relationId);
+
+  deleteUserRelation(initiator, initiatorRelation);
+  deleteUserRelation(affected, affectedRelation);
+
+  await initiator.save();
+  await affected.save();
+};
+
+// ====================================
+// HELPERS
+// ====================================
+const getUserRelationPair = async (userId: number, relationId: number) => {
+  const user = await User.findById(userId, "relations");
+  if (!user) {
+    throw new Error();
+  }
+
+  const relation = user.relations.find(
+    (relation) => relation._id === relationId
+  );
+  if (!relation) {
+    throw new Error();
+  }
+
+  return { user, relation };
+};
+
+const deleteUserRelation = (user: IUser, relation: IRelation) => {
+  user.relations.filter((rel) => rel !== relation);
 };
