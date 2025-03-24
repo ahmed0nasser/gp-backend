231e16757d533cd2d45c13db4cf511195cdedb78
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-03-24 02:14:14 +0200

    Add paging to getNotifications service

--- src/app/services/notifications.ts
+++ src/app/services/notifications.ts
@@ -3,12 +3,24 @@ import { INotification } from "../schemas/database/notification";
 
 export const getNotificationsByUserId = async (
   userId: number,
+  page: number,
   size: number
 ): Promise<INotification[]> => {
   const user = await User.findById(userId, "notifications");
   if (!user) {
     throw new Error();
   }
-
-  return user.notifications.slice(0, size);
+  const pagesNum = getNotificationsPagesNum(user.notifications.length);
+  if (page > pagesNum) {
+    throw new Error();
+  }
+  
+  return user.notifications.slice((page - 1) * 50, size);
 };
+
+// ====================================
+// HELPERS
+// ====================================
+const getNotificationsPagesNum = (len: number) => {
+  return Math.ceil(len / 50);
+}
