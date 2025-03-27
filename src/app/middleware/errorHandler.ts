487c9155f82d48a89b42126d9867eae02fe53e0a
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-03-27 04:00:17 +0200

    Update errorHandler to accomodate for server errors

--- src/app/middleware/errorHandler.ts
+++ src/app/middleware/errorHandler.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express";
 import APIError from "../errors/APIError";
+import ServerError from "../errors/ServerError";
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   if (res.headersSent) {
@@ -7,6 +8,8 @@ const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   }
   if (err instanceof APIError) {
     res.status(err.statusCode).json({ status: "error", error: err.errRes });
+  } else if (err instanceof ServerError) {
+    res.status(500).json({ status: "error", error: { message: err.message } });
   } else {
     res
       .status(500)
