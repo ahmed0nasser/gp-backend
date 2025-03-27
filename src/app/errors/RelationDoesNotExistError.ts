4eae722cdd3c7d0b2d84b0372956c4d334762795
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-03-27 17:07:31 +0200

    Give 404 for relation does not exist

--- src/app/errors/RelationDoesNotExistError.ts
+++ src/app/errors/RelationDoesNotExistError.ts
@@ -2,7 +2,7 @@ import APIError from "./APIError";
 
 class RelationDoesNotExistError extends APIError {
   constructor(userId: number, relationId: number) {
-    super(401, {
+    super(404, {
       message: "Invalid relation id",
       details: `user with id=${userId} does not have relation with id=${relationId}`,
     });
