0adea6d7f01a2ddddf5ce82e108e180c6892764f
ahmed0nasser <ahmed.n.abdulmajeed@gmail.com>
2025-04-14 00:08:49 +0200

    Add automatic timestamps to notification DB schema

--- src/app/schemas/database/notification.ts
+++ src/app/schemas/database/notification.ts
@@ -7,19 +7,20 @@ export interface INotification {
   type: NotificationType;
   title: string;
   body: string;
-  timestamp: Date;
   sender: string;
   isRead: boolean;
 }
 
-const notificationSchema = new Schema<INotification>({
-  _id: { type: Number, required: true },
-  type: { type: String, required: true },
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now() },
-  sender: String,
-  isRead: { type: Boolean, default: false },
-});
+const notificationSchema = new Schema<INotification>(
+  {
+    _id: { type: Number, required: true },
+    type: { type: String, required: true },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    sender: String,
+    isRead: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 export default notificationSchema;
